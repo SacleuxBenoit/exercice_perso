e9e679fdaab571a06417e1107a37a830b17e854e
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-11-18 19:11:41 +0100

    add the algo for the BubbleSort in script.js in random+BubbleSort

--- BubbleSort/random+BubbleSort/js/script.js
+++ BubbleSort/random+BubbleSort/js/script.js
@@ -1,3 +1,13 @@
+const BubbleSort = arr => {
+    for (i = 0; i < arr.length - 1; i++) {
+        for (j = 0; j < arr.length - (i + 1); i++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    } return arr;
+};
+
 function RandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
