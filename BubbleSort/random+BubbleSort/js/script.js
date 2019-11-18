3350c20f56ec61d0ffbfdf9f59970954d46dd17e
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-11-18 19:07:38 +0100

    add 3 const, (a,b and c) with the random and add another const for the array in script.js in random+BubbleSort

--- BubbleSort/random+BubbleSort/js/script.js
+++ BubbleSort/random+BubbleSort/js/script.js
@@ -3,3 +3,9 @@ function RandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const a = RandomInt(1, 1000);
+const b = RandomInt(1, 1000);
+const c = RandomInt(1, 1000);
+
+const arr = [a, b, c]
