8e64f5c2264e488faafeecdd12c0de2fc6bd09d3
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-11-18 19:15:02 +0100

    resolve a problem with the BubbleSort (j++ instead of i++) in script.js in random+BubbleSort

--- BubbleSort/random+BubbleSort/js/script.js
+++ BubbleSort/random+BubbleSort/js/script.js
@@ -1,6 +1,6 @@
 const BubbleSort = arr => {
     for (i = 0; i < arr.length - 1; i++) {
-        for (j = 0; j < arr.length - (i + 1); i++) {
+        for (j = 0; j < arr.length - (i + 1); j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
             }
