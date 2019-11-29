9bbd6dd80192ea61a066d28b073bd3e6836f8a6f
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-11-29 10:28:17 +0100

    add do while in script.js

--- MoreOrLess/script.js
+++ MoreOrLess/script.js
@@ -6,3 +6,17 @@ function entierAleatoire(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 let entier = entierAleatoire(1, 2000);
+let ask = "Entrez un nombre :";
+
+
+do {
+    answer = prompt(ask);
+    if (answer < entier) {
+        alert("Votre nombres est inférieur");
+    }
+
+    else {
+        alert("Votre nombre est supérieur");
+    }
+
+} while (answer != entier)
