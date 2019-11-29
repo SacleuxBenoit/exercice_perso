1a7416d89cef607fbbff38ffacb92e1910f17429
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-11-29 10:33:14 +0100

    reorganisation of script.js and add a some parameters for the counter

--- MoreOrLess/script.js
+++ MoreOrLess/script.js
@@ -5,15 +5,17 @@ function entierAleatoire(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 let entier = entierAleatoire(1, 2000);
+
 let ask = "Entrez un nombre :";
+let answer;
 let compteur = 0;
-
-
 
 do {
     answer = prompt(ask);
     compteur++;
+
     if (answer < entier) {
         alert("Votre nombres est inférieur");
     }
@@ -23,3 +25,13 @@ do {
     }
 
 } while (answer != entier)
+
+if ('${compteur} > 2 && <10') {
+    alert(`nice one, vous avez gagné en ${compteur} coups ! `)
+}
+else if ('${compteur} > 11 && =< 20') {
+    alert(`peut mieux faire, vous avez gagné en ${compteur} coups ! `)
+}
+else {
+    alert(`Vous avez gagné en ${compteur} coups !  Félicitations !`);
+}
